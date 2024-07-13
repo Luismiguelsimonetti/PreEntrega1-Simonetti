d043da50eb6b1079d6a1c240fdeca031960d0785
LuisSimonetti <luismiguelsimonetti@gmail.com>
2024-07-13 20:22:04 +0100

    primera parte

---
+++ src/components/CartWidget.jsx
@@ -0,0 +1,7 @@
+import carrito  from "../assets/cart.png";
+
+export const CartWidget = () => ( 
+    <>
+    < img src= {carrito} />5
+    </>
+    );
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,21 @@
+import { CartWidget } from "./CartWidget";
+
+export const NavBar = () => (
+
+<>
+<h6>Wimi Pasteleria Artesanal</h6>
+
+<ul>
+    <li>
+        <a href="#">Alfajores</a>
+    </li>
+    <li>
+        <a href="#">Cookies</a>
+    </li>
+    <li>
+        <a href="#">Conitos</a>
+    </li>
+</ul>
+<CartWidget/>
+</>
+);
